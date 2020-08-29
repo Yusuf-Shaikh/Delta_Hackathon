d0ca7469520e2b4a84048672bfbb33a0a83321a9
Yusuf-Shaikh <62813708+Yusuf-Shaikh@users.noreply.github.com>
2020-08-29 18:24:28 +0530

    final

--- app/src/main/java/com/example/photoediter_application/EditorActivity.java
+++ app/src/main/java/com/example/photoediter_application/EditorActivity.java
@@ -3,23 +3,258 @@ package com.example.photoediter_application;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Point;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.SeekBar;
 
 import com.bumptech.glide.Glide;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class EditorActivity extends AppCompatActivity {
 
+    RelativeLayout relativeLayout;
     ImageView imageView;
+    Button square,circle,triangle,save;
+    private Bitmap bitmap,originalBitmap,filteredBitmap,finalBitmap;
+    private SeekBar brightness, contrast, saturation;
+    private float mbrightness, mcontrast, msaturation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
-
+        imageView = findViewById(R.id.image_view);
         Intent intent = getIntent();
         String path = intent.getStringExtra("path");
 
-        imageView = findViewById(R.id.image);
-        Glide.with(EditorActivity.this).load(path).centerCrop().into(imageView);
+        imageView = findViewById(R.id.image_view);
+        square = findViewById(R.id.add_image);
+        circle = findViewById(R.id.add_sticker);
+        triangle = findViewById(R.id.add_text);
+        relativeLayout = findViewById(R.id.rel1);
+        save = findViewById(R.id.save);
+
+        bitmap = BitmapFactory.decodeFile(path);
+        originalBitmap = bitmap;
+
+        square.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bitmap = originalBitmap;
+                bitmap = getSquareBitmap(bitmap);
+                imageView.setImageBitmap(bitmap);
+                relativeLayout.setVisibility(View.GONE);
+
+            }
+        });
+
+        circle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bitmap = originalBitmap;
+                bitmap = getCircularBitmap(bitmap);
+                imageView.setImageBitmap(bitmap);
+                relativeLayout.setVisibility(View.GONE);
+            }
+        });
+
+        triangle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bitmap = originalBitmap;
+                bitmap = getTriangularBitmap(bitmap);
+                imageView.setImageBitmap(bitmap);
+                relativeLayout.setVisibility(View.GONE);
+            }
+        });
+
+        imageView.setImageBitmap(bitmap);
+
+        contrast = (SeekBar) findViewById(R.id.seekbar_contrast);
+        mcontrast = contrast.getProgress();
+
+        brightness = (SeekBar) findViewById(R.id.seekbar_brightness);
+        mbrightness = brightness.getProgress();
+
+        saturation = (SeekBar) findViewById(R.id.seekbar_saturation);
+        msaturation = saturation.getProgress();
+
+        contrast.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
+                mcontrast = progress;
+                imageView.setImageBitmap(changeBitmapContrast(bitmap, (float) mcontrast / 255f, (float) mbrightness / 255f));
+                originalBitmap = Bitmap.createBitmap(bitmap);
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BitmapDrawable draw = (BitmapDrawable) imageView.getDrawable();
+                Bitmap bitmap = draw.getBitmap();
+
+                FileOutputStream outStream = null;
+                File sdCard = Environment.getExternalStorageDirectory();
+                File dir = new File(sdCard.getAbsolutePath() + "/YourFolderName");
+                dir.mkdirs();
+                String fileName = String.format("%d.jpg", System.currentTimeMillis());
+                File outFile = new File(dir, fileName);
+                try {
+                    outStream = new FileOutputStream(outFile);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outStream);
+                try {
+                    outStream.flush();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    outStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
     }
+
+    protected Bitmap getCircularBitmap(Bitmap srcBitmap) {
+        // Calculate the circular bitmap width with border
+        int squareBitmapWidth = Math.min(srcBitmap.getWidth(), srcBitmap.getHeight());
+        // Initialize a new instance of Bitmap
+        Bitmap dstBitmap = Bitmap.createBitmap (
+                squareBitmapWidth, // Width
+                squareBitmapWidth, // Height
+                Bitmap.Config.ARGB_8888 // Config
+        );
+        Canvas canvas = new Canvas(dstBitmap);
+        // Initialize a new Paint instance
+        Paint paint = new Paint();
+        paint.setAntiAlias(true);
+        Rect rect = new Rect(0, 0, squareBitmapWidth, squareBitmapWidth);
+        RectF rectF = new RectF(rect);
+        canvas.drawOval(rectF, paint);
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        // Calculate the left and top of copied bitmap
+        float left = (squareBitmapWidth-srcBitmap.getWidth())/2;
+        float top = (squareBitmapWidth-srcBitmap.getHeight())/2;
+        canvas.drawBitmap(srcBitmap, left, top, paint);
+        // Free the native object associated with this bitmap.
+        srcBitmap.recycle();
+        // Return the circular bitmap
+        return dstBitmap;
+    }
+
+    protected Bitmap getSquareBitmap(Bitmap srcBitmap) {
+        // Calculate the circular bitmap width with border
+        int squareBitmapWidth = Math.min(srcBitmap.getWidth(), srcBitmap.getHeight());
+        // Initialize a new instance of Bitmap
+        Bitmap dstBitmap = Bitmap.createBitmap (
+                squareBitmapWidth, // Width
+                squareBitmapWidth, // Height
+                Bitmap.Config.ARGB_8888 // Config
+        );
+        Canvas canvas = new Canvas(dstBitmap);
+        // Initialize a new Paint instance
+        Paint paint = new Paint();
+        paint.setAntiAlias(true);
+        Rect rect = new Rect(0, 0, squareBitmapWidth, squareBitmapWidth);
+        RectF rectF = new RectF(rect);
+        canvas.drawRect(0,0,squareBitmapWidth,squareBitmapWidth,paint);
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        // Calculate the left and top of copied bitmap
+        float left = (squareBitmapWidth-srcBitmap.getWidth())/2;
+        float top = (squareBitmapWidth-srcBitmap.getHeight())/2;
+        canvas.drawBitmap(srcBitmap, left, top, paint);
+        // Free the native object associated with this bitmap.
+        srcBitmap.recycle();
+        // Return the circular bitmap
+        return dstBitmap;
+    }
+
+    protected Bitmap getTriangularBitmap(Bitmap srcBitmap) {
+        int squareBitmapWidth = Math.min(srcBitmap.getWidth(), srcBitmap.getHeight());
+
+        Bitmap dstBitmap = Bitmap.createBitmap (
+                squareBitmapWidth, // Width
+                squareBitmapWidth, // Height
+                Bitmap.Config.ARGB_8888 // Config
+        );
+        Canvas canvas = new Canvas(dstBitmap);
+
+        Paint paint = new Paint();
+        paint.setStyle(Paint.Style.FILL_AND_STROKE);
+        paint.setAntiAlias(true);
+
+        Path path = new Path();
+        path.setFillType(Path.FillType.EVEN_ODD);
+        path.moveTo(0,squareBitmapWidth);
+        path.lineTo(squareBitmapWidth,squareBitmapWidth);
+        path.lineTo(squareBitmapWidth/2,0);
+        path.lineTo(0,squareBitmapWidth);
+        path.close();
+
+        canvas.drawPath(path, paint);
+
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+
+        float left = (squareBitmapWidth-srcBitmap.getWidth())/2;
+        float top = (squareBitmapWidth-srcBitmap.getHeight())/2;
+        canvas.drawBitmap(srcBitmap, left, top, paint);
+        srcBitmap.recycle();
+        return dstBitmap;
+    }
+
+    public static Bitmap changeBitmapContrast(Bitmap bmp, float contrast, float brightness)
+    {
+        ColorMatrix cm = new ColorMatrix(new float[]
+                {
+                        contrast, 0, 0, 0, brightness,
+                        0, contrast, 0, 0, brightness,
+                        0, 0, contrast, 0, brightness,
+                        0, 0, 0, 1, 0
+                });
+
+        Bitmap ret = Bitmap.createBitmap(bmp.getWidth(), bmp.getHeight(), bmp.getConfig());
+
+        Canvas canvas = new Canvas(ret);
+
+        Paint paint = new Paint();
+        paint.setColorFilter(new ColorMatrixColorFilter(cm));
+        canvas.drawBitmap(bmp, 0, 0, paint);
+
+        return ret;
+    }
+
 }
